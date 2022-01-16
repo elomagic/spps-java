71f0007db8040648ed742c018bdf78d6580b942f
C. Rambow <dev@elomagic.de>
2022-01-16 15:54:22 +0100

    Doc added

--- spps-shared/src/main/java/de/elomagic/spps/shared/SimpleCryptProvider.java
+++ spps-shared/src/main/java/de/elomagic/spps/shared/SimpleCryptProvider.java
@@ -47,6 +47,14 @@ public abstract class SimpleCryptProvider {
     protected static final AtomicReference<Path> SETTINGS_FILE = new AtomicReference<>(DEFAULT_SETTINGS_FILE);
 
     // TODO JavaDoc
+
+    /**
+     * Creates a random AES key with 256 ke size.
+     *
+     * @return Returns the key
+     * @throws SimpleCryptException Thrown when something went wring on creation of the key.
+     */
+    @NotNull
     protected abstract Key createPrivateKey() throws SimpleCryptException;
 
     /**
